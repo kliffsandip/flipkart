ee7e74c9ee4b26887b45b71ba12bcc5ec06eb0ca
jk <jskitcha@gmail.com>
2019-09-13 11:37:12 +0530

    cloninig with sandip ekart repo

--- newtext.sh
+++ newtext.sh
@@ -3,4 +3,9 @@ this is very important
 have a great day
 how is the class?
 good
-i am going to try some cherry pick command here 
+i
+am going to try some cherry pick command here
+
+hi sandip 
+jai here
+
